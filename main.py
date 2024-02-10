cf00b7f824af46ea385eac077e4030911093a309
Reus <victor.villalva2003@hotmail.com>
2024-02-10 00:27:23 -0600

    Create buttons with color red,blue,green

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ ventana = Tk()
 ventana.title('Paint')
 ventana.geometry('1100x600')
 
+# ---------------- Variables --------------------
 # Valor de color
 strokeColor = StringVar()
 strokeColor.set('black')
@@ -16,16 +17,14 @@ finalPoint = [0,0]
 #Valor grosor
 strokeSize = IntVar()
 strokeSize.set(1)
-options = [1,5,10,15,20]
+options = [1,2,3,5,10]
 
-
-
+# ---------------- Funciones --------------------
 def pencil():
     strokeColor.set('black')
 def eraser():
     strokeColor.set('white')
     canvas["cursor"] = DOTBOX
-
 def selectColor():
     selectedColor =  colorchooser.askcolor('red', title='Seleccionar Color')
 
@@ -33,14 +32,11 @@ def selectColor():
         strokeColor.set('black')
     else:
         strokeColor.set(selectedColor[1])
-
-
 def paint(e):
     global initialPoint,finalPoint
     x = e.x
     y = e.y
     finalPoint = [x,y]
-
 
     if initialPoint != [0,0] :
         canvas.create_line(initialPoint[0], initialPoint[1], finalPoint[0], finalPoint[1], fill=strokeColor.get(), width=strokeSize.get())
@@ -50,8 +46,7 @@ def paint(e):
     if e.type == '5':
         initialPoint = [0,0]
 
-
-
+# ---------------- Interfas --------------------
 # Section 1 - Tools
 section1 = Frame(ventana, height=100, width=1100)
 section1.grid(row=0,column=0,sticky=NW)
@@ -89,6 +84,16 @@ colorFrame.grid(row=0, column=2)
 colorBoxButton = Button(colorFrame, text='Seleccionar Color', width=15, command=selectColor)
 colorBoxButton.grid(row=0, column=0)
 
+# Frame - Colores Destacados
+colorsFrame = Frame(section1, height=100, width=100, relief=SUNKEN, borderwidth=3)
+colorsFrame.grid(row=0, column=3)
+
+redButton = Button(colorsFrame, text='Rojo', bg='red', fg='white', command=lambda: strokeColor.set('red'), width=10)
+redButton.grid(row=0, column=0)
+blueButton = Button(colorsFrame, text='Azul', bg='blue', fg='white', command=lambda: strokeColor.set('blue'), width=10)
+blueButton.grid(row=1, column=0)
+greenButton = Button(colorsFrame, text='Verde', bg='green', fg='white', command=lambda: strokeColor.set('green'), width=10)
+greenButton.grid(row=2, column=0)
 
 # Section 2 - Canvas
 section2 = Frame(ventana,height=500,width=1100,bg="yellow")
@@ -101,7 +106,6 @@ canvas.grid(row=0,column=0)
 
 canvas.bind("<B1-Motion>", paint)
 canvas.bind("<ButtonRelease-1>", paint)
-
 ventana.resizable(False,False)
 ventana.mainloop()
 
